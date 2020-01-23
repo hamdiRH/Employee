1afd8a871942ef46bb1ada249c96a070db43c83a
hamdi <hamdirahal@gmail.com>
2020-01-23 11:45:37 +0100

    fix

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,5 +1,5 @@
 import { createSelector } from "reselect";
 import { initialState } from "./reducer";
 
-const selectEmployee = (state,id) => state.data.find(el => el.id === +id) || initialState;
+const selectEmployee = (state,id) => state.data.find(el => el.id === Number(id)) || initialState;
 export const selectOneEmployes = createSelector([selectEmployee],employe=>employe)
